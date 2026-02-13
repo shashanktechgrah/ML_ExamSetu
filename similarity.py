17b659e9c9102b5c5d23b9c99569bf82f559aacf
shashanktechgrah <shashanktechgrah@gmail.com>
2026-02-13 16:08:04 +0530

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -1,12 +1,13 @@
 from sentence_transformers import SentenceTransformer, util
 
-model = SentenceTransformer("all-mpnet-base-v2", device="cpu")
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def semantic_similarity(correct: str, student: str) -> float:
     emb1 = model.encode(correct, convert_to_tensor=True)
     emb2 = model.encode(student, convert_to_tensor=True)
     score = util.cos_sim(emb1, emb2).item()
     return round(score, 2)
+
 
 
 
